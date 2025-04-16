80e32997fe224dc0b6e4418f872622e7c639387f
Anconeyra <anconeyrafsuyo@gmail.com>
2025-04-16 09:21:00 -0500

    Creacion del proyecto y acceso

---
+++ Gestión de Inventarios/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Gestión_de_Inventarios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
